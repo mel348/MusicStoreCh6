a79df99b71b2a28997a94a1cbaa80a9a3c0bcd89
Mel <emanm@mail.nmc.edu>
2022-10-27 23:58:42 -0400

    Add project files.

---
+++ Controllers/ArtistController.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using MusicStore.Models;
+
+namespace MusicStore.Controllers
+{
+    public class ArtistController : Controller
+    {
+        private readonly MusicContext _context;
+
+        public ArtistController(MusicContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Artist
+        [Route("Artists/list/[action]/sort_enter_1_or_2/{ArtistSort?}")]  
+        //Artists, list, sort_enter_1_or_2 is STATIC 
+        //Action and ArtistSort? are DYNAMIC
+        public IActionResult Index(int ArtistSort=0)   //set to zero incase they don't pass anything along.
+        {
+            //allows the users to sort by artist in ascending or descending order.)
+            if (ArtistSort == 2)
+                return View(_context.Artists.OrderByDescending(a => a.Name).ToList());  //if they send 2 we sort in descending order
+            else if (ArtistSort == 1)
+                return View(_context.Artists.OrderBy(a => a.Name).ToList());        //if they send 1 we just display the one
+            else
+                return View(_context.Artists.ToList());     //if they pass nothing we do not sort anything
+            //return View(await _context.Artists.ToListAsync());
+        }
+
+        // GET: Artist/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var artist = await _context.Artists
+                .FirstOrDefaultAsync(m => m.ArtistId == id);
+            if (artist == null)
+            {
+                return NotFound();
+            }
+
+            return View(artist);
+        }
+
+        // GET: Artist/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Artist/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ArtistId,Name")] Artist artist)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(artist);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(artist);
+        }
+
+        // GET: Artist/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var artist = await _context.Artists.FindAsync(id);
+            if (artist == null)
+            {
+                return NotFound();
+            }
+            return View(artist);
+        }
+
+        // POST: Artist/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ArtistId,Name")] Artist artist)
+        {
+            if (id != artist.ArtistId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(artist);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ArtistExists(artist.ArtistId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(artist);
+        }
+
+        // GET: Artist/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var artist = await _context.Artists
+                .FirstOrDefaultAsync(m => m.ArtistId == id);
+            if (artist == null)
+            {
+                return NotFound();
+            }
+
+            return View(artist);
+        }
+
+        // POST: Artist/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var artist = await _context.Artists.FindAsync(id);
+            _context.Artists.Remove(artist);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ArtistExists(int id)
+        {
+            return _context.Artists.Any(e => e.ArtistId == id);
+        }
+    }
+}
---
+++ ScaffoldingReadMe.txt
@@ -0,0 +1,43 @@
+﻿Scaffolding has generated all the files and added the required dependencies.
+
+However the Application's Startup code may required additional changes for things to work end to end.
+Add the following code to the Configure method in your Application's Startup class if not already done:
+
+        app.UseMvc(routes =>
+        {
+          routes.MapRoute(
+            name : "areas",
+            template : "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+          );
+        });
+       
+       *****B. Associating controllers with an area
+To connect a controller to an area, you can use the Area attribute
+
+When using the Area attribute, you must use the areaName that you specified in the startup.cs file.
+
+Example using the Admin areaName:
+
+      namespace GuitarShop.Areas.Admin.Controllers {
+     		 [Area("Admin")]
+      	 public class HomeController : Controller {
+     				 public IActionResult Index() {
+      				return View();  // maps to /Areas/Admin/Views/Home/Index.cshtml 
+					}
+			 }
+		 }
+
+         YOU HAVE TO HAVE AREAS/ADMIN IN YOUR AREA ADMIN VIEWS IN ORDER FOR THE ROUTING TO WORK.
+
+        *** If you want to specify an area in the route attribute, you need to use [area] in the same way you used [controller] and [action]
+
+Example:
+
+      [Area("Admin")]
+      public class ProductController : Controller {
+		   [Route("[area]/[controller]s/{id?}")] 
+			public IActionResult List(string id = "All")  {...};
+    		public IActionResult Add() {...}; 
+			public IActionResult Update(int id) {...}; 
+ 			public IActionResult Delete(int id) {...};
+		}
